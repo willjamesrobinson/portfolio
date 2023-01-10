0628bd1b170a3b1111e0ebc528a6aac1e3ae8b30
willjamesrobinson <willjamesr@gmail.com>
2023-01-10 21:04:44 +1100

    minor touch ups

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,7 +8,7 @@ import {AiFillStar} from 'react-icons/ai'
 const About = () => {
   return (
     <section id='about'>
-      <h5>Get To Know</h5>
+      <h2>Get To Know</h2>
       <h2>About Me!</h2>
 
       <div className="container about_container">
